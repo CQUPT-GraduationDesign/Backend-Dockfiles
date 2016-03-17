fc5c1fcb5e26ac5b5ea79cf4bfd90b1631061c65
lk1ngaa7 <lk1ngaa7@gmail.com>
2016-03-17 13:20:06 +0800

    add memcached,update readme,add log for php and php-fpm,update docker-compose.yml

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Backend-Dockfiles
 Dockfiles
 Architecture
-![ Architecture ](http://7j1zp6.com1.z0.glb.clouddn.com/%E6%9E%B6%E6%9E%84%E5%9B%BE.png)
+![ Architecture ](http://7j1zp6.com1.z0.glb.clouddn.com/%E6%9E%B6%E6%9E%84%E5%9B%BE%20-2.png)
 ##USAGE
 ###STEP0
-install docker-engine
-install docker-compose
+install docker-engine (version 1.10 or upper)
+install docker-compose (version 1.6 or upper)
 ###STEP1
+`
 ./init.sh
+`
 ###STEP2
+`
 docker-compose -f ./docker-compose.yml up
+`
+##TIPS
+- Place your code ~/opt/htdocs
+- Log in the ~/opt/log
+- Mysql persistence data in the ~/opt/data/mysql
--- init.sh
+++ init.sh
@@ -1,11 +1,14 @@
 if [ ! -d "~/opt" ]; then
   mkdir ~/opt
 fi
-if [ ! -d "~/opt/nginx" ]; then
-  mkdir ~/opt/nginx
+if [ ! -d "~/opt/log" ]; then
+  mkdir ~/opt/log
 fi
-if [ ! -d "~/opt/nginx/log" ]; then
-  mkdir ~/opt/nginx/log
+if [ ! -d "~/opt/log/nginx" ]; then
+  mkdir ~/opt/log/nginx
+fi
+if [ ! -d "~/opt/log/php" ]; then
+  mkdir ~/opt/log/php
 fi
 if [ ! -d "~/opt/mysql" ]; then
   mkdir ~/opt/mysql
@@ -13,9 +16,18 @@ fi
 if [ ! -d "~/opt/mysql/data" ]; then
   mkdir ~/opt/mysql/data
 fi
-if [ ! -f "~/opt/nginx/log/access.log" ]; then
-  touch ~/opt/nginx/log/access.log
+if [ ! -f "~/opt/log//nginx/access.log" ]; then
+  touch ~/opt/log/nginx/access.log
 fi
-if [ ! -f "~/opt/nginx/log/error.log" ]; then
-  touch ~/opt/nginx/log/error.log
+if [ ! -f "~/opt/log/nginx/error.log" ]; then
+  touch ~/opt/log/nginx/error.log
+fi
+if [ ! -f "~/opt/log/php/error.log" ]; then
+  touch ~/opt/log/php/error.log
+fi
+if [ ! -f "~/opt/log/php/fpm-error.log" ]; then
+  touch ~/opt/log/php/fpm-error.log
+fi
+if [ ! -f "~/opt/log/php/fpm-access.log" ]; then
+  touch ~/opt/log/php/fpm-access.log
 fi
